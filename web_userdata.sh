13aba4e5239c6f10fa0aabdd4fddedbb2f43481d
Alonso Braithwaite <160416175+AlonsoBTech@users.noreply.github.com>
2024-04-14 19:51:51 -0400

    Update web_userdata.sh

--- web_userdata.sh
+++ web_userdata.sh
@@ -5,7 +5,6 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-sudo apt-get install mysql-client-core-8.0 -y
 echo "<h1>Welcome to my Web Application</h1>" | sudo tee /var/www/html/index.html
 echo "<p>Instance ID: $INSTANCE_ID </p>" | sudo tee -a /var/www/html/index.html
 echo "<p>Availability Zone: $AVAILABILITY_ZONE </p>" | sudo tee -a /var/www/html/index.html
