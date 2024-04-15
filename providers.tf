e49678634b11ae414abb817d31fecd50f6850b75
Alonso Braithwaite <160416175+AlonsoBTech@users.noreply.github.com>
2024-04-15 17:51:22 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,9 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0"
+      version = "~> 5.0"
     }
   }
+
+    backend "s3" {
+    bucket = "three-tier-terraform-backend"
+      key = "three-tier/terraform.tfstate"
+      region = "ca-central-1"
+    }
 }
 
 provider "aws" {
