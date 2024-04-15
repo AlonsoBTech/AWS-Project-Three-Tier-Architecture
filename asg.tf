7d9dd918edad622183fe34e01627626da27425e9
Alonso Braithwaite <160416175+AlonsoBTech@users.noreply.github.com>
2024-04-15 18:10:08 -0400

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,8 +1,10 @@
+### Creating Web Tier SSH Key
 resource "aws_key_pair" "web_tier_key" {
   key_name   = var.webkey_name
   public_key = file(var.webkey_public_path)
 }
 
+### Creating Web Tier ASG EC2 Launch Template 
 resource "aws_launch_template" "web_tier_launch_tpl" {
   name_prefix   = "web_tier_template"
   image_id      = data.aws_ami.ubuntu.id
@@ -25,6 +27,7 @@ resource "aws_launch_template" "web_tier_launch_tpl" {
   }
 }
 
+### Creating Web Tier ASG 
 resource "aws_autoscaling_group" "web_tier_asg" {
   desired_capacity    = 2
   max_size            = 2
@@ -39,11 +42,13 @@ resource "aws_autoscaling_group" "web_tier_asg" {
   }
 }
 
+### Creating App Tier SSH Key
 resource "aws_key_pair" "app_tier_key" {
   key_name   = var.appkey_name
   public_key = file(var.appkey_public_path)
 }
 
+### Creating App Tier ASG EC2 Launch Template 
 resource "aws_launch_template" "app_tier_launch_tpl" {
   name_prefix   = "app_tier_template"
   image_id      = data.aws_ami.ubuntu.id
@@ -66,6 +71,7 @@ resource "aws_launch_template" "app_tier_launch_tpl" {
   }
 }
 
+### Creating App Tier ASG 
 resource "aws_autoscaling_group" "app_tier_asg" {
   desired_capacity    = 2
   max_size            = 2
